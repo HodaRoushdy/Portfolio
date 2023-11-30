8e7b924f5398bd82e3abfade078a72e96aae1b4d
HodaRoushdy <hodaroshdy106@gmail.com>
2023-12-01 01:17:39 +0200

    test

--- src/app/_not-found/dist/page.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function NotFound() {
-    return (React.createElement("div", null,
-        React.createElement("h1", null, "error")));
-}
-exports["default"] = NotFound;
